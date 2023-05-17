a99e34504512d65f16b307f043c834c514fbd830
Gabriel Andrade <gandrade.dev@gmail.com>
2023-05-16 21:51:39 -0300

    New: criado frontend

---
+++ web/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="h-screen bg-zinc-950 text-zinc-50">
+      Hello World!
+      <h1 className="text-4xl font-bold"> Sua cápsula do tempo</h1>
+    </div>
+  )
+}
