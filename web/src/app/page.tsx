96f4d93353d8443d97236c4054eda9ffee989e2c
Gabriel Andrade <gandrade.dev@gmail.com>
2023-05-20 19:07:10 -0300

    New: estrutura visual da Home

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,8 +1,21 @@
 export default function Home() {
   return (
-    <div className="h-screen bg-zinc-950 text-zinc-50">
-      Hello World!
-      <h1 className="text-4xl font-bold"> Sua cápsula do tempo</h1>
-    </div>
+    <main className="grid min-h-screen grid-cols-2">
+      {/* Left */}
+      <div className="flex flex-col items-start justify-between px-28 py-16"></div>
+
+      {/* Right */}
+      <div className="flex flex-col p-16">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="w-[360px] text-center leading-relaxed">
+            Você ainda não registrou nenhuma lembrança, comece a{' '}
+            <a href="" className="underline hover:text-gray-50">
+              criar agora
+            </a>
+            !
+          </p>
+        </div>
+      </div>
+    </main>
   )
 }
