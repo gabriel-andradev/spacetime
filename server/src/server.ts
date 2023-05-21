09c98bad9d5417ea9ced24459909648aa9aad555
Gabriel Andrade <gandrade.dev@gmail.com>
2023-05-20 21:03:30 -0300

    plugin cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,10 @@
 import fastify from 'fastify'
+import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
 
+app.register(cors, { origin: true })
 app.register(memoriesRoutes)
 
 app
