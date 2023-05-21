42ed52a31f36fd2d1410fba9f9b44840e2f1b537
Gabriel Andrade <gandrade.dev@gmail.com>
2023-05-21 18:36:25 -0300

    New: token JWT

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -60,6 +60,17 @@ export async function authRoutes(app: FastifyInstance) {
       })
     }
 
-    return { user }
+    const token = app.jwt.sign(
+      {
+        name: user.name,
+        avatar_url: user.avatarUrl,
+      },
+      {
+        sub: user.id,
+        expiresIn: '30 days',
+      },
+    )
+
+    return { token }
   })
 }
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,21 @@
 import 'dotenv/config'
 
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 import fastify from 'fastify'
 import { authRoutes } from './routes/auth'
 import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
 
-app.register(cors, { origin: true })
+app.register(cors, {
+  origin: true,
+})
 app.register(memoriesRoutes)
 app.register(authRoutes)
+app.register(jwt, {
+  secret: 'spacetimeDEV',
+})
 
 app
   .listen({
