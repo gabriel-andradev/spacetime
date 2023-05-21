330a654eddd53dd2662c3fbeb4fcee4174d60c08
Gabriel Andrade <gandrade.dev@gmail.com>
2023-05-21 18:06:23 -0300

    New: env e componentes

---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,15 @@
+export function Copyright() {
+  return (
+    <>
+      Feito com 💙 por{' '}
+      <a
+        target="_blank"
+        className="underline hover:text-gray-50"
+        href="https://github.com/gabriel-andradev"
+        rel="noreferrer"
+      >
+        gandrade.dev
+      </a>
+    </>
+  )
+}
