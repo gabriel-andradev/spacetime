33102844f4de1b081c37c98263b2801b1aa0e259
Gabriel Andrade <gandrade.dev@gmail.com>
2023-05-21 18:28:44 -0300

    New: rota de autenticacao

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,15 @@
-import fastify from 'fastify'
+import 'dotenv/config'
+
 import cors from '@fastify/cors'
+import fastify from 'fastify'
+import { authRoutes } from './routes/auth'
 import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
 
 app.register(cors, { origin: true })
 app.register(memoriesRoutes)
+app.register(authRoutes)
 
 app
   .listen({
