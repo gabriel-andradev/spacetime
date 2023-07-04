6a806a01a20e11570ae1ef92434b29baba58512e
Gabriel Andrade <gandrade.dev@gmail.com>
2023-07-04 20:21:24 -0300

    New: new page memories and auth middleware

--- web/src/components/Hero.tsx
+++ web/src/components/Hero.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import nlwLogo from '../assets/nlw-spacetime-logo.svg'
+import Link from 'next/link'
 
 export function Hero() {
   return (
@@ -16,12 +17,12 @@ export function Hero() {
         </p>
       </div>
 
-      <a
+      <Link
         className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600"
-        href=""
+        href="/memories/new"
       >
         CADASTRAR LEMBRANÇA
-      </a>
+      </Link>
     </div>
   )
 }
