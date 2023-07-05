c2ffa31bcb54216d07cb15b95f712bbc18993711
Gabriel Andrade <gandrade.dev@gmail.com>
2023-07-04 21:16:13 -0300

    New: route upload and static archive

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,16 +3,27 @@ import 'dotenv/config'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
 import fastify from 'fastify'
+import multipart from '@fastify/multipart'
 import { authRoutes } from './routes/auth'
 import { memoriesRoutes } from './routes/memories'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastify()
+
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true,
 })
-app.register(memoriesRoutes)
 app.register(authRoutes)
+app.register(uploadRoutes)
+app.register(memoriesRoutes)
 app.register(jwt, {
   secret: 'spacetimeDEV',
 })
