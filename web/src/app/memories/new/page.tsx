86b1b92110e7dac699b48997972d2cfc263e076e
Gabriel Andrade <gandrade.dev@gmail.com>
2023-07-08 17:00:25 -0300

    New: layout list img and api searching memories

--- web/src/app/memories/new/page.tsx
+++ web/src/app/memories/new/page.tsx
@@ -1,11 +1,10 @@
-'use client'
 import { NewMemoryForm } from '@/components/NewMemoryForm'
 import { ChevronLeft } from 'lucide-react'
 import Link from 'next/link'
 
 export default function NewMemory() {
   return (
-    <div className="flex flex-1 flex-col gap-4">
+    <div className="flex flex-1 flex-col gap-4 p-16">
       <Link
         href="/"
         className="flex items-center gap-1 text-sm text-gray-200 hover:text-gray-100"
